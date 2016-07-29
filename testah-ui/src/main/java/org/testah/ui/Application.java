13f0d1d45b1707b171268f558500a301223023d4
Jason Palmer <jason.palmer2008@gmail.com>
2016-07-29 02:05:32 -0400

    WIP App does start now.

---
+++ testah-ui/src/main/java/org/testah/ui/Application.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.testah.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JASON
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ testah-ui/src/main/java/org/testah/ui/model/Testplan.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.testah.ui.model;
+
+import java.math.BigInteger;
+
+/**
+ * @author JASON
+ *
+ */
+public class Testplan {
+	private BigInteger id;
+	private String name;
+	/**
+	 * @return the id
+	 */
+	public BigInteger getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
