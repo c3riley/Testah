32b125a70741babf6bc2ef008b7ec913175fdd73
criley-cazena <criley@cazena.com>
2016-09-30 08:30:41 -0400

    rev

--- testah-junit/src/main/java/org/testah/framework/cli/Cli.java
+++ testah-junit/src/main/java/org/testah/framework/cli/Cli.java
@@ -42,7 +42,7 @@ public class Cli {
     private final Params opt;
 
     /** The Constant version. */
-    public static final String version = "0.6.1";
+    public static final String version = "0.6.2";
 
     /** The Constant BAR_LONG. */
     public static final String BAR_LONG = "=============================================================================================";
