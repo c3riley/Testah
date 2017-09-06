ba4fe35324434eb84f12afc5f08ff24e11f9afae
user <user@Gregors-Mini.home>
2017-09-05 20:57:14 -0400

    increment version number

--- testah-junit/src/main/java/org/testah/framework/cli/Cli.java
+++ testah-junit/src/main/java/org/testah/framework/cli/Cli.java
@@ -42,7 +42,7 @@ public class Cli {
     private Params opt;
 
     /** The Constant version. */
-    public static final String version = "0.7.0";
+    public static final String version = "0.8.2";
 
     /** The Constant BAR_LONG. */
     public static final String BAR_LONG = "=============================================================================================";
