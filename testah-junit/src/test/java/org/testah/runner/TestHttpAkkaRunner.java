2ccb406842d8ae5c406731cd19d614f3165c3ad1
Collin Riley <criley@eagleinvsys.com>
2017-09-15 09:34:07 -0400

    fix findbugs

--- testah-junit/src/test/java/org/testah/runner/TestHttpAkkaRunner.java
+++ testah-junit/src/test/java/org/testah/runner/TestHttpAkkaRunner.java
@@ -6,19 +6,19 @@ import org.testah.driver.http.requests.GetRequestDto;
 public class TestHttpAkkaRunner {
 
     @Test
-    public void InvalidUrl() {
+    public void invalidUrl() {
         final HttpAkkaRunner a = HttpAkkaRunner.getInstance();
         a.runAndReport(5, new GetRequestDto("htp:/www.goeeeeogle.com"), 5);
     }
 
     @Test
-    public void WrongUrl() {
+    public void wrongUrl() {
         final HttpAkkaRunner a = HttpAkkaRunner.getInstance();
         a.runAndReport(5, new GetRequestDto("http://www.goeeeeogle.com"), 5);
     }
 
     @Test
-    public void HappyPath() {
+    public void happyPath() {
         final HttpAkkaRunner a = HttpAkkaRunner.getInstance();
         a.runAndReport(5, new GetRequestDto("http://www.google.com"), 5);
     }
