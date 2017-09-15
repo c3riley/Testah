582fc512d696619f37e0edeb9867408cf46cb24c
Collin Riley <c3riley@hotmail.com>
2017-09-15 09:45:57 -0400

    Fix/reports not opening (#79)
    * fix for reports not opening, fix for akka http bad url, add stats lib
    
    * fix findbugs

--- testah-junit/src/main/java/org/testah/runner/testPlan/TestPlanActor.java
+++ testah-junit/src/main/java/org/testah/runner/testPlan/TestPlanActor.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Set;
 
 public class TestPlanActor extends UntypedActor {
-    private static List<ResultDto> results = new ArrayList<ResultDto>();
+    private static List<ResultDto> results = null;
     private final ActorRef workerRouter;
     private final int nrOfWorkers;
 
@@ -26,7 +26,7 @@ public class TestPlanActor extends UntypedActor {
     public void onReceive(final Object message) throws Exception {
         if (message instanceof ResultDto) {
             TS.log().info(Thread.currentThread().getId());
-            results.add((ResultDto) message);
+            getResults().add((ResultDto) message);
         } else if (message instanceof Set) {
             for (final Class<?> test : (Set<Class<?>>) message) {
                 workerRouter.tell(test, getSelf());
@@ -55,7 +55,7 @@ public class TestPlanActor extends UntypedActor {
     }
 
     public static void resetResults() {
-        results = new ArrayList<ResultDto>();
+        results = null;
     }
 
 }
--- testah-junit/src/test/java/org/testah/runner/TestHttpAkkaRunner.java
+++ testah-junit/src/test/java/org/testah/runner/TestHttpAkkaRunner.java
@@ -1,14 +1,24 @@
 package org.testah.runner;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.testah.driver.http.requests.GetRequestDto;
 
 public class TestHttpAkkaRunner {
 
-    @Ignore
     @Test
-    public void test() {
+    public void invalidUrl() {
+        final HttpAkkaRunner a = HttpAkkaRunner.getInstance();
+        a.runAndReport(5, new GetRequestDto("htp:/www.goeeeeogle.com"), 5);
+    }
+
+    @Test
+    public void wrongUrl() {
+        final HttpAkkaRunner a = HttpAkkaRunner.getInstance();
+        a.runAndReport(5, new GetRequestDto("http://www.goeeeeogle.com"), 5);
+    }
+
+    @Test
+    public void happyPath() {
         final HttpAkkaRunner a = HttpAkkaRunner.getInstance();
         a.runAndReport(5, new GetRequestDto("http://www.google.com"), 5);
     }
