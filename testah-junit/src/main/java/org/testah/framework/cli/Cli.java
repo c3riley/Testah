723a0014d083a8ecbe4c4abded4945c3bb93c6d7
Collin Riley <criley@eagleinvsys.com>
2017-09-18 09:45:20 -0400

    add message for same json asserts

--- testah-junit/src/main/java/org/testah/framework/cli/Cli.java
+++ testah-junit/src/main/java/org/testah/framework/cli/Cli.java
@@ -52,7 +52,7 @@ public class Cli {
     /**
      * The Constant version.
      */
-    public static final String version = "0.9.0";
+    public static final String version = "0.9.1";
 
     /**
      * The Constant BAR_LONG.
