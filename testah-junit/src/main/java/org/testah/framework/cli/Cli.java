2898cc22b44e6cd2a386554e7798e09caaef4e43
Gregor Weiske <gweiske@eagleinvsys.com>
2017-10-17 08:55:19 -0400

    update version in Cli.java

--- testah-junit/src/main/java/org/testah/framework/cli/Cli.java
+++ testah-junit/src/main/java/org/testah/framework/cli/Cli.java
@@ -52,7 +52,7 @@ public class Cli {
     /**
      * The Constant version.
      */
-    public static final String version = "0.9.3";
+    public static final String version = "0.9.7";
 
     /**
      * The Constant BAR_LONG.
