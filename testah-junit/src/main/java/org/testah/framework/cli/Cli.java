d00bbf706ac89d92bba8815f5e350dfd91f6108a
gweiske <gcweiske@gmail.com>
2017-11-08 14:35:30 -0500

    fix bug with verbose asserts using hamcrest matcher (#101)
    * fix chunk execution time collection
    
    * increment testah version, add comment
    
    * fix bug with verbose asserts using hamcrest matcher
    
    * increment version

--- testah-junit/src/main/java/org/testah/framework/cli/Cli.java
+++ testah-junit/src/main/java/org/testah/framework/cli/Cli.java
@@ -53,7 +53,7 @@ public class Cli {
     /**
      * The Constant version.
      */
-    public static final String version = "0.9.17";
+    public static final String version = "0.9.18";
 
     /**
      * The Constant BAR_LONG.
