7feba82bc7ec62adadff38a762c9c1c8de760db4
Collin Riley <criley@eagleinvsys.com>
2018-01-26 11:19:00 -0500

    fix double encoding json report

--- testah-junit/src/main/java/org/testah/framework/cli/Cli.java
+++ testah-junit/src/main/java/org/testah/framework/cli/Cli.java
@@ -53,7 +53,7 @@ public class Cli {
     /**
      * The Constant version.
      */
-    public static final String version = "0.10.0";
+    public static final String version = "0.10.1";
 
     /**
      * The Constant BAR_LONG.
