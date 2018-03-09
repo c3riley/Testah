3f15b87db570354deaa61f98558231bbf34af3fa
sbhark <shunmok@shunny.kr>
2018-03-09 16:53:16 -0500

    updates to the dto to return date in a specified format

--- testah-junit/src/main/java/org/testah/client/dto/RunTimeDto.java
+++ testah-junit/src/main/java/org/testah/client/dto/RunTimeDto.java
@@ -1,6 +1,8 @@
 package org.testah.client.dto;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * The Class RunTimeDto.
@@ -8,14 +10,29 @@ import java.util.Date;
 public class RunTimeDto {
 
     /**
+     * Date format
+     */
+    private SimpleDateFormat dateFormat;
+
+    /**
      * The start time.
      */
     private Long startTime = null;
 
     /**
+     * The start date.
+     */
+    private Date startDate = null;
+
+    /**
      * The end time.
      */
     private Long endTime = null;
+
+    /**
+     * The end date.
+     */
+    private Date endDate = null;
 
     /**
      * The duration.
@@ -26,7 +43,8 @@ public class RunTimeDto {
      * Instantiates a new run time dto.
      */
     public RunTimeDto() {
-
+        this.dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSZ");
+        this.dateFormat.setTimeZone(TimeZone.getTimeZone("America/New_York"));
     }
 
     /**
@@ -35,8 +53,9 @@ public class RunTimeDto {
      * @return the run time dto
      */
     public RunTimeDto start() {
-        return start(new Date().getTime());
-
+        Date startDate = new Date();
+        this.startDate = startDate;
+        return start(startDate.getTime());
     }
 
     /**
@@ -56,7 +75,9 @@ public class RunTimeDto {
      * @return the run time dto
      */
     public RunTimeDto stop() {
-        return stop(new Date().getTime());
+        Date endDate = new Date();
+        this.endDate = endDate;
+        return stop(endDate.getTime());
     }
 
     /**
@@ -104,5 +125,17 @@ public class RunTimeDto {
     public Long getEndTime() {
         return endTime;
     }
+
+    /**
+     * Returns the start date in the preset dateformat
+     * @return
+     */
+    public String getStartDate() { return this.dateFormat.format(this.startDate); }
+
+    /**
+     * Returns the end date in the preset dateformat
+     * @return
+     */
+    public String getEndDate() { return this.dateFormat.format(this.endDate); }
 
 }
