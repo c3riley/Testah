6f4c216c390400769d4a9a3cf122bd930da671b3
Shun Mok Bhark <sxb5828@g.rit.edu>
2018-03-12 12:01:24 -0400

    Add start/end date objects to RunTimeDTO (#116)
    * add date
    
    * add Testah.iml to gitignore
    
    * update readme to execute gradlew in testah-junit to build full project
    
    * adding missing getters
    
    * adding missing getters
    
    * adding elastic search index schema for testplan storage
    
    * updates to the dto to return date in a specified format
    
    * fix unit test, add params for timezone and date formate, add simple unit test.
    
    * bump version

--- testah-junit/src/main/java/org/testah/client/dto/RunTimeDto.java
+++ testah-junit/src/main/java/org/testah/client/dto/RunTimeDto.java
@@ -1,6 +1,12 @@
 package org.testah.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.commons.lang.StringUtils;
+import org.testah.TS;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * The Class RunTimeDto.
@@ -8,14 +14,30 @@ import java.util.Date;
 public class RunTimeDto {
 
     /**
+     * Date format.
+     */
+    @JsonIgnore
+    private SimpleDateFormat dateFormat;
+
+    /**
      * The start time.
      */
     private Long startTime = null;
 
     /**
+     * The start date.
+     */
+    private Date startDate = null;
+
+    /**
      * The end time.
      */
     private Long endTime = null;
+
+    /**
+     * The end date.
+     */
+    private Date endDate = null;
 
     /**
      * The duration.
@@ -26,7 +48,12 @@ public class RunTimeDto {
      * Instantiates a new run time dto.
      */
     public RunTimeDto() {
-
+        this.dateFormat = new SimpleDateFormat(TS.params().getTimeFormat());
+        if (StringUtils.isEmpty(TS.params().getTimezone())) {
+            this.dateFormat.setTimeZone(TimeZone.getDefault());
+        } else {
+            this.dateFormat.setTimeZone(TimeZone.getTimeZone(TS.params().getTimezone()));
+        }
     }
 
     /**
@@ -35,8 +62,9 @@ public class RunTimeDto {
      * @return the run time dto
      */
     public RunTimeDto start() {
-        return start(new Date().getTime());
-
+        Date startDate = new Date();
+        this.startDate = startDate;
+        return start(startDate.getTime());
     }
 
     /**
@@ -56,7 +84,9 @@ public class RunTimeDto {
      * @return the run time dto
      */
     public RunTimeDto stop() {
-        return stop(new Date().getTime());
+        Date endDate = new Date();
+        this.endDate = endDate;
+        return stop(endDate.getTime());
     }
 
     /**
@@ -103,6 +133,24 @@ public class RunTimeDto {
      */
     public Long getEndTime() {
         return endTime;
+    }
+
+    /**
+     * Returns the start date in the preset dateformat.
+     *
+     * @return start date
+     */
+    public String getStartDate() {
+        return this.dateFormat.format(this.startDate);
+    }
+
+    /**
+     * Returns the end date in the preset dateformat.
+     *
+     * @return end date
+     */
+    public String getEndDate() {
+        return this.dateFormat.format(this.endDate);
     }
 
 }
