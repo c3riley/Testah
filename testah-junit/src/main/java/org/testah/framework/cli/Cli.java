ab179c5f9c31f4a44711ab15f62515e74637de8b
Gregor Weiske <gweiske@eagleinvsys.com>
2021-08-02 18:23:39 -0400

    return the start, stop times, number of sent and received requests; reset the received count

--- testah-junit/src/main/java/org/testah/framework/cli/Cli.java
+++ testah-junit/src/main/java/org/testah/framework/cli/Cli.java
@@ -44,7 +44,7 @@ public class Cli {
      * The Constant version.
      */
 
-    public static final String version = "3.3.4";
+    public static final String version = "3.3.5";
 
     /**
      * The Constant BAR_LONG.
