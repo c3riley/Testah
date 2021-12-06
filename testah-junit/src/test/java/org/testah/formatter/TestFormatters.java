d04f19731ea742850bf1ea6eb72c9d25f0e6d837
xbbnl3r <gregor.weiske@bnymellon.com>
2021-12-06 16:23:55 -0500

    Use hash of test case name as id avoid character issues in testcase links in report

--- testah-junit/src/test/java/org/testah/formatter/TestFormatters.java
+++ testah-junit/src/test/java/org/testah/formatter/TestFormatters.java
@@ -23,7 +23,7 @@ public class TestFormatters extends BrowserTestPlan {
     @Test
     @TestCase(description = "THIS IS A JUST TEST")
     public void test1() {
-        TS.browser().goTo("http://www.google.com");
+        TS.browser().goTo("https://www.google.com");
         TS.asserts().isFalse(false);
     }
 
@@ -31,7 +31,7 @@ public class TestFormatters extends BrowserTestPlan {
     @Test(expected = AssertionError.class)
     @TestCase(description = "THIS IS A JUST TEST")
     public void test2() {
-        TS.browser().goTo("http://www.google.com");
+        TS.browser().goTo("https://www.google.com");
         TS.asserts().isFalse(true);
     }
 
