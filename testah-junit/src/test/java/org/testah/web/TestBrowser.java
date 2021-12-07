b1af56bfdfb3161d88740cf407fdc82ac071cc24
xbbnl3r <gregor.weiske@bnymellon.com>
2021-12-07 05:55:38 -0500

    Revert "Use hash of test case name as id avoid character issues in testcase links in report"
    This reverts commit 88b0f833a8f3f83d24c80d314113fdcf0ebbd5c4.

---
+++ testah-junit/src/test/java/org/testah/web/TestBrowser.java
@@ -0,0 +1,168 @@
+package org.testah.web;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.testah.TS;
+import org.testah.client.enums.BrowserType;
+import org.testah.driver.web.browser.AbstractBrowser;
+import org.testah.driver.web.browser.FirefoxBrowser;
+import org.testah.driver.web.browser.GoogleChromeBrowser;
+import org.testah.driver.web.browser.PhantomJsBrowser;
+import org.testah.driver.web.element.AbstractWebElementWrapper;
+import org.testah.framework.annotations.TestCase;
+import org.testah.framework.annotations.TestPlan;
+import org.testah.framework.testPlan.BrowserTestPlan;
+
+import java.io.File;
+import java.util.List;
+import java.util.Locale;
+
+@TestPlan
+public class TestBrowser extends BrowserTestPlan {
+
+    private final String baseUrl = "https://www.google.com/";
+    private final String baseTitle = "Google";
+
+    @Before
+    public void setup() {
+        TS.browser().goTo(baseUrl);
+    }
+
+    @After
+    public void tearDown() {
+        TS.browser().close();
+    }
+
+    @TestCase
+    @Test
+    public void testPageTitle() {
+        TS.browser().getWebElement(By.name("q"), 30).waitTillIsDisplayed();
+        TS.browser().waitForTitle(baseTitle, 20);
+        TS.asserts().equalsTo(baseTitle, TS.browser().getTitle());
+        TS.browser().assertTitle(baseTitle);
+    }
+
+    @TestCase
+    @Test
+    public void testPageUtl() {
+        TS.asserts().equalsTo(baseUrl, TS.browser().getUrl());
+        TS.browser().assertUrl(baseUrl);
+    }
+
+    @TestCase
+    @Test
+    public void testScreenShot() {
+        final String screenshot = TS.browser().takeScreenShot();
+        final File f = new File(TS.params().getOutput(), screenshot);
+        TS.asserts().notNull(f);
+        step("step 2");
+        TS.asserts().isTrue(f.exists());
+        step("step 3");
+        TS.asserts().isFalse(f.isDirectory());
+        step("step 4");
+        TS.asserts().isTrue(screenshot.endsWith(".png"));
+
+    }
+
+    @TestCase
+    @Test
+    public void testGetDriver() {
+        TS.asserts().notNull(TS.browser().getDriver());
+        TS.asserts().equalsTo(baseTitle, TS.browser().getDriver().getTitle());
+    }
+
+    @TestCase
+    @Test
+    public void testGetJavaScriptValue() {
+        final String rtn = TS.browser().getJavaScriptValue("return 'cool';");
+        TS.asserts().equalsTo(rtn, "cool");
+    }
+
+    @TestCase
+    @Test
+    public void testElementWaitTime() {
+        final int rtn = TS.browser().getElementWaitTime();
+        TS.asserts().equalsTo(10, rtn);
+        TS.browser().setElementWaitTime(2);
+        TS.asserts().equalsTo(2, TS.browser().getElementWaitTime());
+        TS.browser().setElementWaitTime(10);
+    }
+
+    @TestCase
+    @Test
+    public void testGetCapabilities() {
+        TS.asserts().notNull(TS.browser().getCapabilities());
+        TS.asserts().equalsTo(BrowserType.getBrowserType(TS.browser().getCapabilities().getBrowserName()),
+            TS.params().getBrowser());
+    }
+
+    @TestCase
+    @Test
+    public void testWebElements() {
+        final List<AbstractWebElementWrapper> lst = TS.browser().getWebElements(By.id("spch"));
+        TS.asserts().notNull(lst);
+        TS.asserts().equalsTo(1, lst.size());
+        TS.asserts().notNull(lst.get(0));
+        TS.asserts().equalsTo(By.id("spch"), lst.get(0).getBy());
+        TS.asserts().notNull(lst.get(0).getDriverWebElement());
+        TS.asserts().notNull("style value is not null", lst.get(0).getAttribute("style"));
+    }
+
+    @TestCase
+    @Test
+    public void testWebElement() {
+        final AbstractWebElementWrapper e = TS.browser().getWebElement(By.id("spch"));
+        TS.asserts().notNull(e);
+        TS.asserts().equalsTo(By.id("spch"), e.getBy());
+        TS.asserts().notNull(e.getDriverWebElement());
+        TS.asserts().notNull("style value is not null", e.getAttribute("style"));
+    }
+
+    @Ignore
+    @Test
+    @TestCase(name = "test3")
+    public void testPhantomJs() {
+
+        final PhantomJsBrowser b = new PhantomJsBrowser();
+        b.start().getDriver().get("https://www.google.com");
+        b.goToAndWaitForTitleToChange("https://www.google.com");
+        b.close();
+        TS.asserts().isTrue(false);
+
+    }
+
+    @Test
+    @TestCase(name = "test3")
+    public void testChrome() {
+
+        final AbstractBrowser<GoogleChromeBrowser> b = new GoogleChromeBrowser().start();
+        b.start().getDriver().get("https://www.google.com");
+        b.goToAndWaitForTitleToChange("https://www.google.com");
+        b.close();
+
+    }
+
+    @Test
+    @TestCase(name = "test2", runTypes = "FIREFOX")
+    public void testFirefox() {
+
+        final FirefoxBrowser b = new FirefoxBrowser();
+        b.start().getDriver().get("https://www.google.com");
+        b.goToAndWaitForTitleToChange("https://www.google.com");
+        b.close();
+
+    }
+
+    @Test
+    @TestCase(name = "testScreenshot")
+    public void testScreenshot() {
+        String browserContent = TS.browser().goTo("https://www.google.com").getHtml().toLowerCase(Locale.ROOT);
+        TS.asserts().contains("", browserContent, "</title>");
+        TS.asserts().contains("", browserContent, "google");
+        TS.asserts().contains("", browserContent, "</body>");
+    }
+
+}
---
+++ testah-junit/src/test/java/org/testah/web/TestWebElementWrapper.java
@@ -0,0 +1,92 @@
+package org.testah.web;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.testah.TS;
+import org.testah.driver.web.element.AbstractWebElementWrapper;
+import org.testah.framework.annotations.TestCase;
+import org.testah.framework.annotations.TestPlan;
+import org.testah.framework.testPlan.BrowserTestPlan;
+
+@TestPlan
+public class TestWebElementWrapper extends BrowserTestPlan {
+
+    private static final String SRC = "src";
+    private static final String baseUrl =
+        "http://htmlpreview.github.io/?https://raw.githubusercontent.com/SeleniumHQ/" +
+            "selenium/master/common/src/web/clicks.html";
+    private static final String formPage = "http://htmlpreview.github.io/?https://raw.githubusercontent.com/" +
+        "SeleniumHQ/selenium/master/common/src/web/formPage.html";
+
+    /**
+     * Test setup.
+     */
+    @Before
+    public void setup() {
+        TS.browser().goTo(baseUrl);
+        TS.util().pause(2000L, "Since using git redirect need to wait a little while");
+
+    }
+
+    @After
+    public void tearDown() {
+        TS.browser().close();
+    }
+
+    @TestCase
+    @Test
+    public void happy() {
+        TS.asserts().isTrue(true);
+    }
+
+    @Ignore
+    @TestCase
+    @Test
+    public void testWebElementAsserts() {
+        final AbstractWebElementWrapper e = TS.browser().getWebElement(By.id("enclosed-image"));
+        e.assertAttributeExists(SRC);
+        e.assertAttributeValue(SRC,
+            "https://raw.githubusercontent.com/SeleniumHQ/selenium/master/common/src/web/icon.gif");
+        e.assertFound();
+    }
+
+    @Ignore
+    @TestCase
+    @Test
+    public void testWebElementVerify() {
+        final AbstractWebElementWrapper e = TS.browser().getWebElement(By.id("enclosed-image"));
+
+        TS.asserts().isTrue(e.verifyAttributeValue(SRC,
+            "https://raw.githubusercontent.com/SeleniumHQ/selenium/master/common/src/web/icon.gif"));
+        TS.asserts().isTrue(e.verifyFound());
+        TS.asserts().isFalse(e.verifyNotfound());
+        TS.asserts().isFalse(e.verifyAttributeValue(SRC, "shouldNotFind"));
+    }
+
+    @Ignore
+    @TestCase
+    @Test
+    public void testWebElementWithin() {
+        TS.browser().goToAndWaitForTitle(
+            "http://htmlpreview.github.io/?https://raw.githubusercontent.com/SeleniumHQ/selenium/master/common/src/web/xhtmlTest.html",
+            "XHTML Test Page");
+        TS.browser().getWebElement(By.id("username")).assertFound().click().clearText()
+            .assertAttributeValue("value", "").typeText("hello").assertAttributeValue("value", "hello")
+            .assertTypeText("cool");
+    }
+
+    @Ignore
+    @TestCase
+    @Test
+    public void testWithin() {
+        TS.browser().goToAndWaitForTitle(
+            "http://htmlpreview.github.io/?https://raw.githubusercontent.com/SeleniumHQ/selenium/master/common/src/web/xhtmlTest.html",
+            "XHTML Test Page");
+        TS.browser().getWebElement(By.cssSelector("div.content")).getElementWithIn(By.id("username")).assertFound()
+            .assertTypeText("cool1");
+    }
+
+}
